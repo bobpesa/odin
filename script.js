69fce4a42019d689feb9b7ad044de06d71880322
bobpesa <143897871+bobpesa@users.noreply.github.com>
2025-06-16 07:11:05 +0300

    index and styles

---
+++ script.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const addBookButton = document.getElementById("addBtn")
+// const bookForm = document.querySelector(".bookForm")
+// const inputTitle = document.getElementById("title")
+// const inputPages = document.getElementById("pages")
+// const inputwriterAuthor = document.getElementById("writerAuthor")
+// const saveButton = document.getElementById("saveBtn")
+// let cards = []
+
+
+
+// addBookButton.onclick = addBtn
+// saveButton.onclick = saveBtn
+
+// function addBtn() {
+//     addBookButton.classList.toggle("active")
+//     bookForm.classList.toggle("active")
+//     console.log("eeeee")
+// }
+
+// function saveBtn() {
+//     let Title = inputTitle.value.trim();
+//     let Pages = inputPages.value.trim();
+//     // let writerAuthor = inputwriterAuthor.value.trim()
+
+//     const authorDiv = document.createElement("div")
+//     authorDiv.classList.add("card")
+
+//     const detailsDiv = document.createElement("div")
+//     detailsDiv.classList.add("bookDetails")
+
+//     const details = document.createElement("div")
+//     details.classList.add("details")
+
+//     const bookTitle = document.createElement("h1")
+//     bookTitle.classList.add("bookTitle")
+//     bookTitle.textContent = Title
+
+//     const noPages = document.createElement("p")
+//     noPages.classList.add("noPages")
+//     noPages.textContent = Pages
+
+//     // const Author = document.createElement("p")
+//     // Author.classList.add("author")
+//     // Author.textContent = Author
+
+//     const deletew = document.createElement("div")
+//     deletew.classList.add("deletew")
+
+//     const readBtn = document.createElement("button")
+//     readBtn.textContent = "Read"
+
+//     const deleteBtn = document.createElement("button")
+//     deleteBtn.textContent = "Delete"
+
+//     authorDiv.appendChild(detailsDiv)
+
+//     detailsDiv.appendChild(details)
+//     detailsDiv.appendChild(deletew)
+
+//     details.appendChild(bookTitle)
+//     details.appendChild(noPages)
+//     // details.appendChild(Author)
+
+
+//     deletew.appendChild(readBtn)
+//     deletew.appendChild(deleteBtn)
+
+//     console.log(Title, Pages)
+//     bookForm.classList.toggle("active")
+// }
+
